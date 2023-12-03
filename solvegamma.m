8b3355c02d1494b8cafd3744d30760739680f190
unknown <alansh2@illinois.edu>
2023-12-02 23:23:45 -0600

    bugfixes in geom and LLT

--- solvegamma.m
+++ solvegamma.m
@@ -11,22 +11,24 @@ function [Gsol,E] = solvegamma(G0,alpha,vij,un,ua,zeta,af)
 %    Gsol: computed solution for Gamma/Vinf
 %       E: magnitude of the residual
 
-N = length(G0);
+N = length(G0)/2;
 uinf = [cosd(alpha) 0 sind(alpha)];
 
-[Gsol,E] = fminunc(@objfun,G0);
+[Gsol,E] = fminunc(@objfun,G0(1:N));
+Gsol = [Gsol;flipud(Gsol)];
 
-    function fval = objfun(G)
+    function fval = objfun(Ghalf)
+        G = [Ghalf;flipud(Ghalf)];
         v = zeros(N,3);
         for i = 1:3
-            v(:,i) = uinf(i) + vij(:,:,i)*G/(4*pi);
+            v(:,i) = uinf(i) + vij(1:N,:,i)*G/(4*pi);
         end
-        alf = atan2d(dot(v,un,2),dot(v,ua,2));
+        alf = atan2d(dot(v,un(1:N,:),2),dot(v,ua(1:N,:),2));
         Cl = zeros(N,1);
         for i = 1:N
             Cl(i) = Panel2D(af,alf(i));
         end
-        fval = mean((2*vecnorm(cross(v,zeta,2),2,2).*G - Cl).^2);
-        % fval = max(abs(2*vecnorm(cross(v,zeta,2),2,2).*G - Cl));
+        fval = mean((2*vecnorm(cross(v,zeta(1:N,:),2),2,2).*Ghalf - Cl).^2);
+        % fval = max(abs(2*vecnorm(cross(v,zeta(1:N,:),2),2,2).*Ghalf - Cl));
     end
 end
