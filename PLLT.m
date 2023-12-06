f8b1c59e60297346b25457e2939c342ace61c89a
alansh2 <alansh2@illinois.edu>
2023-12-05 19:40:13 -0600

    fixed more bugs, sweep and dihedral effect still weird

--- PLLT.m
+++ PLLT.m
@@ -4,19 +4,20 @@ close all
 %% Generate airfoil
 Au = [0.18362 0.33139 0.2805 0.24597];      % upper surface weights
 Al = [-0.18362 -0.20378 -0.17535 -0.12035]; % lower surface weights
+% Au = -Al;
 af = CST_airfoil(Au,Al,51);
 
 %% Wing geometry
 Nhalf = 10;                     % number of horseshoe vortices in a semispan
 b = 10;                         % wingspan
-Lambda = 0;                    % c/4 sweep angle (deg)
+Lambda = 20;                    % c/4 sweep angle (deg)
 phi = 0;                       % dihedral angle (deg)
 ys = linspace(0,1,Nhalf+1);     % non-dimensional semispan coordinate of legs
 % ys = cos(linspace(pi/2,0,Nhalf+1));
-twist = linspace(3,-1,Nhalf+1);  % twist distribution on ys (deg)
+twist = linspace(0,0,Nhalf+1);  % twist distribution on ys (deg)
 % twist = 0.6*(2/pi^2*sqrt(1-ys.^2)+1/pi/8)*180/pi;
-chord = 4*sqrt(1-ys.^2);         % chord distribution on ys
-% chord = linspace(5,3,Nhalf+1);
+% chord = 2*sqrt(1-ys.^2);         % chord distribution on ys
+chord = linspace(4,1.5,Nhalf+1);
 
 [vertex,pctrl,cctrl] = geom2grid(b,chord,Lambda,phi,twist);
 
@@ -29,7 +30,7 @@ daspect([1 1 1])
 %% PLLT
 N = 2*Nhalf;
 
-alpha = 5;
+alpha = 0;
 uinf = [cosd(alpha) 0 sind(alpha)];
 Vinf = 1;
 
@@ -53,12 +54,12 @@ for j = 1:N
         if i == j
             vij(i,j,:) = cross(uinf,r2)/(R2*(R2-dot(uinf,r2))) +...
                 (R1+R2)*cross(r1,r2)/(R1*R2*(R1*R2+dot(r1,r2))) -...
-                cross(uinf,r1)/(R1*(R1-dot(uinf,r2)));
+                cross(uinf,r1)/(R1*(R1-dot(uinf,r1)));
             un(i,:) = cross(r2,r1);
             ua(i,:) = r1 - 0.5*dl(i,:);
         else
             vij(i,j,:) = cross(uinf,r2)/(R2*(R2-dot(uinf,r2))) -...
-                cross(uinf,r1)/(R1*(R1-dot(uinf,r2)));
+                cross(uinf,r1)/(R1*(R1-dot(uinf,r1)));
         end
     end
 end
@@ -72,9 +73,9 @@ a0 = (Cl2 - Cl1)*180/pi; % 2D lift curve slope
 alfZL = -Cl1/a0;
 A = diag(2*vecnorm(cross(repmat(uinf,N,1),zeta,2),2,2),0) - a0/(4*pi)*sum(vij.*reshape(un,1,N,3),3).';
 RHS = a0*(un*uinf.' - alfZL);
-G = A \ RHS;
+G0 = A \ RHS;
 
-[G,E] = solvegamma(G,alpha,vij,un,ua,zeta,af);
+[G,E] = solvegamma(G0,alpha,vij,un,ua,zeta,af);
 
 % Check answer for Gamma/Vinf
 v = zeros(N,3);
@@ -94,3 +95,11 @@ ftest = 2*vecnorm(cross(v,zeta,2),2,2).*G;
 %% Plot
 figure
 plot(pctrl(:,2),G)
+hold on
+plot(pctrl(:,2),G0,'--')
+plot(ys*b/2,G0(Nhalf)*sqrt(1-ys.^2))
+
+figure
+plot(pctrl(:,2),Cl.*cctrl/mean(cctrl))
+hold on
+plot(pctrl(:,2),Cl,'--')
